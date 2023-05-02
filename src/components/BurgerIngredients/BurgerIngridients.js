510fec6f15560802b6bb77c0687f6da557916869
nikita-kechaev <Nkech001@yandex.ru>
2023-05-02 19:08:16 +0300

    add Tab component logic

--- src/components/BurgerIngredients/BurgerIngridients.js
+++ src/components/BurgerIngredients/BurgerIngridients.js
@@ -8,6 +8,7 @@ export default function BurgerIngridients () {
     const [state, setState] = React.useState({
         data: ingridients,
     })
+    const [current, setCurrent] = React.useState('bun')
 
     const ingTypes = [
         {type: 'bun', title: 'Булки'},
@@ -19,13 +20,13 @@ export default function BurgerIngridients () {
         <div className={`${styles.column} mt-10`}>
             <p className="text text_type_main-large">Соберите бургер</p>
             <div className={`${styles.tab} mt-5`}>
-                <Tab value="bun">
+                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
                     Булки
                 </Tab>
-                <Tab value="sauce">
+                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
                     Соусы
                 </Tab>
-                <Tab value="main">
+                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
                     Начинки
                 </Tab>
             </div>
